d0b855cf8368b224e325bfde5fe3468ab03fb90a
Radu Turlacu <turlacu_r@yahoo.com>
2022-04-15 17:24:29 +0000

    Added 'Car Make' and 'Car Model' models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -5,10 +5,14 @@ from django.utils.timezone import now
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake (models.Model):
+    make_name = models.CharField(null=False, max_length=160)
+    make_description = models.TextField()
+
+    def __str__(self):
+        return "Make: " + self.make_name + ", " + \
+               "Description: " + self.make_description
+
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,6 +23,34 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.CharField(null=False, max_length=160)
+    model_name = models.CharField(null=False, max_length=200)
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    COUPE = 'Coupe'
+    CABRIO = 'Cabrio'
+    MODEL_TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (COUPE, 'Coupe'),
+        (CABRIO, 'Cabrio')
+    ]
+    model_type = models.CharField(
+        null=False,
+        max_length=20,
+        choices=MODEL_TYPE_CHOICES,
+        default=SEDAN
+    )
+    model_year = models.DateField(null=True)
+
+    def __str__(self):
+        return  "Model: " + self.model_name + ", " + \
+                "Type: " + self.model_type + ", " + \
+                "Year: " + self.model_year
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
